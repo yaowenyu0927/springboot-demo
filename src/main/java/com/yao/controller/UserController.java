efd9a9ada49b1c4b5a72effe8a7664644522189a
yaowenyu0927 <66705952+yaowenyu0927@users.noreply.github.com>
2021-04-15 17:32:58 +0800

    Update UserController.java

--- src/main/java/com/yao/controller/UserController.java
+++ src/main/java/com/yao/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/to")
 public class UserController {
 
+    //远程操控
     @RequestMapping("/index")
     public String index(Model model){
         model.addAttribute("name","asgdf");
